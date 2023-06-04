7bdbcfd1971fe1db3b6d21c6fc5f427e1c486cd6
blackwolf449 <tacvini@gmail.com>
2023-06-03 21:04:24 -0300

    update technologies and frameworks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btnHome = document.querySelector('.home')
 const btnAbout = document.querySelector('.about')
 const btnProjects = document.querySelector('.projects')
+const btnTech = document.querySelector('.technologies')
 
 function centralizeScroll(contentElement) {
     let elementRect = contentElement.getBoundingClientRect()
@@ -25,3 +26,46 @@ btnAbout.onclick = () => {
 btnProjects.onclick = () => {
     centralizeScroll(document.querySelector('#projects'))
 }
+
+btnTech.onclick = () => {
+    centralizeScroll(document.querySelector('#technologies'))
+}
+
+function createSkills(skills, elementToInsertClass) {
+    let skillElements = ``
+    for (let i = 0; i < skills.length; i++) {
+        if (i + 1 == skills.length) {
+            skillElements += `
+            <div class="skills last-skill">
+                <h3>${skills[i].name}</h3>
+                <img width="70vw" src="${skills[i].svg}"/>
+            </div>`
+        } else {
+            skillElements += `
+                <div class="skills">
+                    <h3>${skills[i].name}</h3>
+                    <img width="70vw" src="${skills[i].svg}"/>
+                </div>`
+        }
+    }
+    document.querySelector(elementToInsertClass).innerHTML = skillElements
+}
+createSkills(
+    [
+        { name: 'HTML', svg: '../assets/html.svg' },
+        { name: 'CSS', svg: '../assets/css.svg' },
+        { name: 'JavaScript (JS)', svg: '../assets/js.svg' },
+        { name: 'VueJS', svg: '../assets/vue.svg' },
+        { name: 'Flutter', svg: '../assets/flutter.svg' },
+    ],
+    '.skill-front'
+)
+createSkills(
+    [
+        { name: 'NodeJS', svg: '../assets/node.svg' },
+        { name: 'Express', svg: '../assets/express.svg' },
+        { name: 'Python', svg: '../assets/python.svg' },
+        { name: 'Postgres', svg: '../assets/postgres.svg' },
+    ],
+    '.skill-back'
+)
